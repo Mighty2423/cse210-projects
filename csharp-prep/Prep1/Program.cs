66bcb698881e0f15aa6bf0984cf092919468826c
Alton <152930905+Mighty2423@users.noreply.github.com>
2025-01-09 21:16:42 -0700

    preps 1-2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask for the user's first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask for the user's last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Ask for the user's age
+        Console.Write("Whats your age? ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        // Display the name in the specified format
+        Console.WriteLine($"Your name is  {firstName} {lastName} ,age {age}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // grade prompt
+        Console.WriteLine("Enter your numerical grade: ");
+        string input = Console.ReadLine();
+        int grade;
+
+        //check if input is a vaild number
+        if (int.TryParse(input, out grade))
+        {
+            if (grade >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else if (grade < 60)
+            {
+                Console.WriteLine("F");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
     }
 }
