1d6f5019aad9a122491ebd8ee77f56c034258689
Alton <152930905+Mighty2423@users.noreply.github.com>
2025-01-11 13:54:39 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //ask the user for their number
+        Console.WriteLine("What is the magic number ? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+
+        //ask the user for their guess
+        Console.WriteLine("What is your guess ? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        //check if the guess is correct
+        if (guess == magicNumber)
+        {
+            Console.WriteLine("You guessed it right!");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it wrong!");
+        }
+
+        //check if the guess is too high
+        if (guess > magicNumber)
+        {
+            Console.WriteLine("Your guess is too high!");
+        }
+
+        //check if the guess is too low
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Your guess is too low!");
+        }
+
+        //loop
+
     }
 }
