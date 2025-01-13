9f80e960e0495ea8c8efc010c1f524e40d5e6556
Mighty2423 <whi23062@byui.edu>
2025-01-13 16:07:57 -0700

    filler

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!!!!!!!!");
+        char[] charArray = { 'H', 'e', 'l', 'l', 'o' };
+        string helloString = new string(charArray);
+        
     }
 }
