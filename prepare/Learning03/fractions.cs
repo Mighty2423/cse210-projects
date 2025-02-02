66a21a3a389433314117eaa7f632b6ebf477ef9f
Alton <152930905+Mighty2423@users.noreply.github.com>
2025-02-02 11:36:33 -0700

    Update fractions.cs

--- prepare/Learning03/fractions.cs
+++ prepare/Learning03/fractions.cs
@@ -2,4 +2,28 @@ using System ;
 
 public class Fraction
 {
+    private int _top;
+    private int _bottom
+    
+    public Fraction()
+    {
+    _top = 1;
+    _bottom = 1;
+    }
+
+    public Fraction(int wholenumber)
+    {
+    _top = int wholenumber;
+    _bottom = 1;
+    }
+
+    public Fraction(int top , int botton)
+    {
+    _top = top;
+    _bottom = bottom;
+    }
+
+
+    
+    
 }
