763549afaad45d19d56081db778a18a46ed335b1
Alton <152930905+Mighty2423@users.noreply.github.com>
2025-02-02 11:38:59 -0700

    Update Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        
+    Fraction start = new Fraction();
+    Console.WriteLine beep;
     }
 }
