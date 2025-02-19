4da8f962d570d2210a3c431062a2d4d219166cb6
Alton <152930905+Mighty2423@users.noreply.github.com>
2025-02-19 14:17:19 -0700

    prompts

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -7,7 +7,11 @@ public class ListingActivity : MindfulnessActivity
     {
         _prompts = new List<string>
         {
-
+            "Who are people that you appreciate?",
+            "What are personal strengths of yours?",
+            "Who are people that you have helped this week?",
+            "When have you felt the Holy Ghost this month?",
+            "Who are some of your personal heroes?"
         };
 
         _responses = new List<string>();
@@ -19,6 +23,9 @@ public class ListingActivity : MindfulnessActivity
     {
         Startactvity();
         PauseAnimation(3);
+        Console. WriteLine("Activity: Listing");
+        Console.WriteLine("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+        
 
         PauseAnimation(3);
 
